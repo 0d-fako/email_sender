250b8b76dfeca3093a22c93a05ea1d97a7f5d8d8
0d-fako <od.fakorede@gmail.com>
2025-04-15 12:56:15 +0100

    Modified Script to send with attachment to multiple emails

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ app_password = os.getenv("APP_PASSWORD")
 yag = yagmail.SMTP(email, app_password)
 
 
-recipient = "od.fakorede@gmail.com"
+recipients = ["od.fakorede@gmail.com", "achlaservices@gmail.com"]
+
 subject = "Hello Olumide"
 body = "Hello! This is a nice to implement feature, being able to send my email from my terminal"
 pdf_attachment = r"C:\Users\Dell\Desktop\Books\Design Thinking\Pains.docx"
 
-yag.send(to=recipient, subject=subject, contents=body, attachments=[pdf_attachment])
+
+for recipient in recipients:
+    yag.send(to=recipient, subject=subject, contents=body, attachments=[pdf_attachment])
 print("Email sent successfully!")
